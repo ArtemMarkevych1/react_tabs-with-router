841fd3e8d877b68da3535c28eeab8e7bb8a522bd
ArtemMarkevych1 <aw.markevich@gmail.com>
2019-10-01 08:55:27 +0300

    Updates

---
+++ precache-manifest.48936b99a9d68ac8300891ea1ec2dee8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f652a0daaec075e589c6f1047a118f77",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "9ad63ae29fd68c193967",
+    "url": "/react_tabs-with-router/static/css/main.2965b766.chunk.css"
+  },
+  {
+    "revision": "ce177d0d1b33968256be",
+    "url": "/react_tabs-with-router/static/js/2.9dd890b4.chunk.js"
+  },
+  {
+    "revision": "9ad63ae29fd68c193967",
+    "url": "/react_tabs-with-router/static/js/main.ef0f3d05.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
